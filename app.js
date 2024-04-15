391c2641c5b16069740b61a6137e5f0eeb4d6f41
Joseph Comyn <joecomyn@hotmail.com>
2024-04-15 16:02:20 +0100

    Added GET:/api which lists all endpoints and purpose/ use of each endpoint with testing

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-const { getTopics } = require('./controller/api-controllers');
+const { getTopics, getEndpoints } = require('./controller/api-controllers');
 
 app.get("/api/topics", getTopics);
+
+app.get('/api', getEndpoints);
 
 app.all('*', (req, res) => {
     res.status(404).send({msg: "Not found: Path doesnt exist"})
--- controller/api-controllers.js
+++ controller/api-controllers.js
@@ -1,8 +1,13 @@
 const { selectTopics } = require('../model/api-models');
+const endpoints = require('../endpoints.json');
 
 exports.getTopics = (req, res, next) => {
     selectTopics()
     .then((topics) => {
         res.status(200).send({ topics });
     })
+};
+
+exports.getEndpoints = (req, res, next) => {
+    res.status(200).send({ endpoints });
 };
